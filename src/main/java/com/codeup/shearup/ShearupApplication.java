737f95d20ff2fea0f03bdf33f6cb323fbab3635f
Chris Eckert <chris.eckert08@gmail.com>
2020-08-14 14:03:01 -0500

    first commit

---
+++ src/main/java/com/codeup/shearup/ShearupApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.shearup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShearupApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ShearupApplication.class, args);
+	}
+	
+}
