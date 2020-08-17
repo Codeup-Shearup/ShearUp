66fe2861010764d332cf4c280bb2ccafb403ab27
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-08-17 16:33:41 -0500

    updated TINYINY to BOOLEAN

--- src/test/java/com/codeup/shearup/models/User.java
+++ src/test/java/com/codeup/shearup/models/User.java
@@ -27,14 +27,74 @@ public class User {
     @Column(nullable = false, length = 50, unique = true)
     private String password;
 
-    @Column(nullable = false, columnDefinition = "TINYINT(1)")
-    private int is_barber;
+    @Column(nullable = false, columnDefinition = "TINYINT")
+    private boolean is_barber;
 
     //NEED HELP WITH FK RELATIONSHIP // may have solved it
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user_id service_id")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "usersId")
     private List<Appointment>  barber_details_id;
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean getIs_barber() {
+        return is_barber;
+    }
+
+    public void setIs_barber(boolean is_barber) {
+        this.is_barber = is_barber;
+    }
+
+    public List<Appointment> getBarber_details_id() {
+        return barber_details_id;
+    }
+
+    public void setBarber_details_id(List<Appointment> barber_details_id) {
+        this.barber_details_id = barber_details_id;
+    }
 }
