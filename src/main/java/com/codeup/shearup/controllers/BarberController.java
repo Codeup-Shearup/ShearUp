39f9f8bd7f7832d40b286d6c9c3d4acd32ed29a8
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-08-19 16:06:30 -0500

    updating small change made to BarberController, cannot recall atm.

--- src/main/java/com/codeup/shearup/controllers/BarberController.java
+++ src/main/java/com/codeup/shearup/controllers/BarberController.java
@@ -35,9 +35,6 @@ public class BarberController {
     }
 
 
-
-
-
     //======ADD A SERVICE PAGE FOR BARBER=========//
     @GetMapping("/barber/add-service")
     public String addService(Model model){
