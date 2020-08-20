6003ba5d4e86ead2eca52c169c889db493a933b0
Chris Eckert <chris.eckert08@gmail.com>
2020-08-20 14:37:30 -0500

    users are programatically logged in upon registration - redirect to home page placeholder

--- src/main/java/com/codeup/shearup/controllers/AuthenticationController.java
+++ src/main/java/com/codeup/shearup/controllers/AuthenticationController.java
@@ -1,5 +1,8 @@
 package com.codeup.shearup.controllers;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,6 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AuthenticationController {
 	@GetMapping("/login")
 	public String showLoginForm() {
-		return "users/login";
+		Authentication token = SecurityContextHolder.getContext().getAuthentication();
+		
+		if (token instanceof AnonymousAuthenticationToken) return "login";
+		
+		return String.format("redirect:%s", "/");
 	}
 }
