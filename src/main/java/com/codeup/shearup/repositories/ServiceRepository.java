cfae8078b629a47a2462b6eabd08737d03a9b2fc
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-08-21 18:19:38 -0500

    added a partial to the profile and add-service page

--- src/main/java/com/codeup/shearup/repositories/ServiceRepository.java
+++ src/main/java/com/codeup/shearup/repositories/ServiceRepository.java
@@ -1,5 +1,6 @@
 package com.codeup.shearup.repositories;
 
+import com.codeup.shearup.models.BarberDetail;
 import com.codeup.shearup.models.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
 
