853c2bf6b1fa017b86445430754a7988b40567da
Knoah Cotto <cotto.knoah@gmail.com>
2020-08-29 15:11:17 -0500

    trying to target all pages background color for consistency

--- src/main/java/com/codeup/shearup/controllers/MapController.java
+++ src/main/java/com/codeup/shearup/controllers/MapController.java
@@ -1,6 +1,8 @@
 package com.codeup.shearup.controllers;
 
 
+import com.codeup.shearup.repositories.BarberDetailRepository;
+import com.codeup.shearup.repositories.LocationRepository;
 import com.codeup.shearup.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,17 +12,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MapController {
 
     private UserRepository usersDao;
+    private BarberDetailRepository barberDetailsDao;
+    private LocationRepository locationsDao;
 
-    public MapController(UserRepository usersDao) {
+//    public MapController(UserRepository usersDao) {
+//        this.usersDao = usersDao;
+//    }
+
+    public MapController(UserRepository usersDao, BarberDetailRepository barberDetailsDao, LocationRepository locationsDao) {
         this.usersDao = usersDao;
+        this.barberDetailsDao = barberDetailsDao;
+        this.locationsDao = locationsDao;
     }
+
 
     @GetMapping("/mapbox-map")
     public String viewMap(Model model) {
         return "mapbox-map";
     }
 
-
+//    @GetMapping("/api/things")
+//    @ResponseBody
+//    public List<Thing> getThings() {
+//        return locationsDao.findAll();
+//    }
 
 
 }
